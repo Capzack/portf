c7e0c0d6b462964304ff21a25b988d4ebe3a2b5f
Capzack <79979128+Capzack@users.noreply.github.com>
2023-04-20 09:09:17 +0300

    Delete kwork_trading_view_tg_signals directory

--- kwork_trading_view_tg_signals/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from api.models import Signal
-from bot_trview.admin_utils import OnlyChange
-
-
-@admin.register(Signal)
-class SignalAdmin(OnlyChange):
-    readonly_fields = ('received_at', 'raw_text', 'pair_with_timeframe', 'type')
--- kwork_trading_view_tg_signals/bot_trview/admin_utils.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class OnlyChange(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
